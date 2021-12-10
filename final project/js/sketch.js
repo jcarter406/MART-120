cd3ca229ae48fe18b998a8a7be1ad8705c234fd5
catatonic2021 <83355378+jcarter406@users.noreply.github.com>
2021-12-10 07:44:42 -0700

    Add files via upload

---
+++ final project/js/sketch.js
@@ -0,0 +1,57 @@
+//let img;
+//let cnv;
+//let c;
+
+//function preload(){
+    //img = loadImage ('assets/kitten.jpg');
+    //}
+
+function setup() {
+    createCanvas(800, 800);
+    //image(img, 0, 0);
+    //img.loadPixels();
+    //c = img.get(img.width / 2, img.height / 2);
+}
+
+function draw() {
+    background (220);
+    //image(img, 25, 25, 50, 50);
+}
+
+    //cnv = createCanvas(img.width, img.height);
+    //print (img.width, img.height);
+    //let newCanvasX = (windowWidth - img.width)/2;
+    //let newCanvasY = (windowHeight - img.height)/2;
+    //cnv.position(newCanvasX, newCanvasY);
+// access the pixel info in image
+   // for(let col = 0; col < img.width; col++){
+       // for(let row = 0; row < img.height; row++){
+          //  let xPos = col;
+          //  let yPos = row;
+         //   let c = img.get(xPos, yPos);
+           // push();
+           // translate(xPos, yPos);
+           // rotate(radians(random(360)));
+           // nofill();
+           // stroke(color(c));
+           // strokeWeight(random(5));
+           // point(xPos, yPos);
+           // stroke(color(c));
+           // strokeWeight(random(5));
+            //curve(x1, y1, x2, y2, x3, y3, x4, y4);
+            //point(col, row);
+            //rect(col, row, 10, 5);
+            //pop();  
+        //}
+    //}
+
+    //function keyPressed(){
+        //if (key === 's'){
+         // saveCanvas("portrait.jpg");
+        //}
+      //}
+
+      function draw() {
+      img = loadImage ('assets/533x800px_kitten.jpg', height = 800, width = 533);
+      }
+
